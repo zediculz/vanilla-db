a2ac0868ec5eb1f1b6e09f5a34389cd7270cfdde
[zedic] <oluwaseyifunmitan1@gmail.com>
2023-08-27 14:33:34 +0100

    0.0.21 with more readable readme.md file update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import { CreateStore } from './utils/db.js'
+import { DB } from './utils/idb.js'
 
 // set, get, sync, remove, length
-
 export const vanillaDb = new CreateStore()
+export const vanillaDB = new DB()
---
+++ src/utils/idb.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-undef */
+// start a db
+// create an object inside the db
+// set and get data stored inside the db objects
+
+export class DB {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {}
+
+  // create db
+  // with db name and version
+  // object name
+  // create db, crate object all from schema
+  create(schema) {
+    this.schema = schema
+    const { db, version, object } = schema
+    const _DB = indexedDB.open(db, version)
+    this._DB = _DB
+
+    _DB.onerror = (err) => console.error('error', err)
+
+    _DB.onupgradeneeded = (e) => {
+      const { name, config } = object
+      const db = e.target.result
+      db.createObjectStore(name, config)
+    }
+  }
+
+  // set data by inserting data into db objects
+  set(name, data) {
+    this._DB.onsuccess = (e) => {
+      const _db = e.target.result
+      const txn = _db.transaction(name, 'readwrite')
+      const store = txn.objectStore(name)
+      const q = store.put(data)
+
+      q.onsuccess = function (e) {
+        return true
+      }
+
+      q.onerror = function (e) {
+        console.log('Error', e)
+      }
+
+      txn.oncomplete = function () {
+        _db.close()
+      }
+    }
+  }
+
+  get(name, id, cb) {
+    this._DB.onsuccess = (e) => {
+      const _db = e.target.result
+      const txn = _db.transaction(name, 'readonly')
+      const store = txn.objectStore(name)
+      const q = store.get(id)
+
+      q.onsuccess = function (e) {
+        cb(e.target.result)
+      }
+
+      q.onerror = function (e) {
+        cb(e)
+      }
+
+      txn.oncomplete = function () {
+        _db.close()
+      }
+    }
+  }
+}
