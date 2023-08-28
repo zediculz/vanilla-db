dc4c4c3d54df0c0e4a83a4efdf3892da235c8860
[zedic] <oluwaseyifunmitan1@gmail.com>
2023-08-28 08:56:02 +0100

    0.0.21.1 ready

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-undef */
 import { CreateStore } from './utils/db.js'
-import { DB } from './utils/idb.js'
 
 // set, get, sync, remove, length
 export const vanillaDb = new CreateStore()
-export const vanillaDB = new DB()
